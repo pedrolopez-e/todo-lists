8428688ec44c29f7c118fb119d905e0ef4050038
Pedro López <pedrolopez.e@hotmail.com>
2022-12-14 10:58:43 -0300

    Create project array, list item object and its functionality

---
+++ scripts/list-functions.js
@@ -0,0 +1,26 @@
+let projects = [];
+
+class TodoItem {
+
+    constructor(title,description,dueDate,priority,difficulty) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.difficulty = difficulty;
+    }
+
+}
+
+function addItem(item){
+    projects.push(item);
+}
+
+function removeItem(item){
+    let index = projects.indexOf(item);
+    if (index == -1) {
+        console.log("There is no such object in the array");
+        return;
+    }
+    projects.splice(index, 1);
+}
