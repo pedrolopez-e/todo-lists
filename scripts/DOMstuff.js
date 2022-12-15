7e82bf9fcb2a1a6681bc5746c7519182cb7d2928
Pedro López <pedrolopez.e@hotmail.com>
2022-12-15 09:10:25 -0300

    Make form appear after clicking add button

--- scripts/DOMstuff.js
+++ scripts/DOMstuff.js
@@ -12,9 +12,48 @@ const projectForm = `<form action="" class="project-form">
 <button class="submit-button">Submit</button>
 </form>`;
 
+const body = document.querySelector('body');
+const addButton = document.querySelector('.add-button');
+
 function renderProjects() {
-    projects.forEach( project => 
-        $(".projects-container").append(`<div class="project" data-id="${project.id}">
-        <h3>${project.title}</h3></div>`)
-        );
+    projects.forEach( project => {
+    }
+    )
 };
+
+addButton.addEventListener('click', () => {
+        let form = document.createElement('form');
+        let formTitle = document.createElement('h2');
+        let projectName = document.createElement('input');
+        let projectDueDate = document.createElement('input');
+        let projectDifficulty = document.createElement('select');
+        let difficultyLabel = document.createElement('label');
+        let easy = document.createElement('option');
+        let medium = document.createElement('option');
+        let hard = document.createElement('option');
+        let submit = document.createElement('button');
+        let formElements = [formTitle,projectName,projectDueDate,difficultyLabel,projectDifficulty];
+        let selectElements = [easy, medium, hard];
+
+        body.appendChild(form);
+        formElements.forEach((element) => {
+            form.appendChild(element);
+        })
+        selectElements.forEach((option) => {
+            projectDifficulty.appendChild(option);
+        })
+
+        formTitle.innerHTML = "New project";
+        projectName.setAttribute("placeholder", "Name");
+        projectDueDate.setAttribute("placeholder", "Due date");
+        projectDifficulty.setAttribute("placeholder", "Difficulty");
+        projectDifficulty.setAttribute("id", "difficulty");
+        difficultyLabel.setAttribute("for", "difficulty");
+        difficultyLabel.innerHTML = "Difficulty: ";
+        easy.setAttribute("value", "easy");
+        easy.innerHTML = "Easy";
+        medium.setAttribute("value", "medium");
+        medium.innerHTML = "Medium";
+        hard.setAttribute("value", "hard");
+        hard.innerHTML = "hard";
+})
